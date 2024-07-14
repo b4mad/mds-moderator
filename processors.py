cd1e7b5416a853de27e11b5675756c6c88be6665
Marcel Hild <hild@b4mad.net>
2024-07-14 12:49:02 +0200

    cleanup

--- processors.py
+++ processors.py
@@ -45,12 +45,6 @@ class ConversationProcessor(LLMResponseAggregator):
         """
         self.user_mapping[user_id] = participant_name
 
-    # def __init__(self, messages: List[dict] = []):
-    #     super().__init__()
-    #     self._messages = messages
-    #     self._aggregation = []
-    #     self._role = "user"
-
     async def process_frame(self, frame: Frame, direction: FrameDirection):
         await super().process_frame(frame, direction)
         if isinstance(frame, self._accumulator_frame):
@@ -65,46 +59,10 @@ class ConversationProcessor(LLMResponseAggregator):
                 }
                 self._aggregation_detailed.append(entry)
 
-    # async def process_frame(self, frame: Frame, direction: FrameDirection):
-    #     await super().process_frame(frame, direction)
-    #     logger.debug(f"ConversationProcessor: {frame}")
-
-    #     if isinstance(frame, UserStoppedSpeakingFrame):
-    #         # Send an app message to the UI
-    #         # await self.push_frame(DailyTransportMessageFrame(CUE_ASSISTANT_TURN))
-    #         # await self.push_frame(DailyTransportMessageFrame(CUE_ASSISTANT_TURN))
-    #         await self._push_aggregation()
-    #     elif isinstance(frame, TranscriptionFrame):
-    #         entry = {
-    #             "user_id": frame.user_id,
-    #             "text": frame.text,
-    #             "timestamp": frame.timestamp
-    #         }
-    #         self._aggregation.append(entry)
-    #     elif isinstance(frame, LLMMessagesFrame):
-    #         # llm response
-    #         logger.debug(f"LLM response: {frame.messages}")
-    #     else:
-    #         # Pass the frame along unchanged
-    #         await self.push_frame(frame, direction)
-
     async def _push_aggregation(self):
         self._aggregation = self.format_aggregation()
         self._aggregation_detailed = []
         await super()._push_aggregation()
-
-    # async def _push_aggregation(self):
-    #     if len(self._aggregation) > 0:
-
-    #         self._messages.append({"role": self._role, "content": self.format_aggregation()})
-
-    #         # Reset the aggregation. Reset it before pushing it down, otherwise
-    #         # if the tasks gets cancelled we won't be able to clear things up.
-    #         self._aggregation = []
-
-    #         frame = LLMMessagesFrame(self._messages)
-    #         logger.debug(f"Pushing LLMMessagesFrame: {self._messages}")
-    #         await self.push_frame(frame)
 
     def format_aggregation(self):
         """
@@ -117,15 +75,3 @@ class ConversationProcessor(LLMResponseAggregator):
             timestamp = entry['timestamp'].strftime("%H:%M:%S")
             formatted.append(f"{timestamp} | {username} | {entry['text']}")
         return "\n".join(formatted)
-
-    def get_conversation_history(self):
-        """
-        Returns the entire conversation history.
-        """
-        return self._messages
-
-    def get_last_n_entries(self, n):
-        """
-        Returns the last n entries of the conversation.
-        """
-        return self._messages[-n:]
