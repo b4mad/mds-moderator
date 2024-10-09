f70097271a5a6f6bb976d3b5ee942ea84fdf05f4
Marcel Hild <hild@b4mad.net>
2024-10-09 17:12:25 +0200

    feat: Add Suspense fallback to Home page

--- frontend/app/page.tsx
+++ frontend/app/page.tsx
@@ -1,11 +1,12 @@
 "use client";
 
-import React from "react";
-
+import React, { Suspense } from "react";
 import App from "../components/App";
 
 export default function Home() {
   return (
-    <App />
+    <Suspense fallback={<div>Loading...</div>}>
+      <App />
+    </Suspense>
   );
 }
